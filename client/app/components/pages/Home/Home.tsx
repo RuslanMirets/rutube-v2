82dea8e014400c69049dd97573ba8b3b2e84bc84
Ruslan <ruslan.mirets@gmail.com>
2022-07-21 08:29:49 +0300

    Save

--- client/app/components/pages/Home/Home.tsx
+++ client/app/components/pages/Home/Home.tsx
@@ -2,12 +2,13 @@ import { FC } from 'react';
 import Layout from '@/components/layout/Layout';
 import Catalog from './catalog/Catalog';
 import Discover from './discover/Discover';
+import { IHome } from './home.interface';
 
-const Home: FC = () => {
+const Home: FC<IHome> = ({ newVideos, randomVideo, topVideo }) => {
 	return (
 		<Layout title='Rutube v2.0 | Видеохостинг'>
-			<Discover />
-			<Catalog />
+			<Discover topVideo={topVideo} randomVideo={randomVideo} />
+			<Catalog newVideos={newVideos} />
 		</Layout>
 	);
 };
